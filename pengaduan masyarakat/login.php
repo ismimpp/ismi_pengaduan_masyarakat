27ad6bb7d350140f785dbe70cf65c89575f10bf7
ismimpp <133618672+ismimpp@users.noreply.github.com>
2023-08-02 11:49:00 +0700

    Add files via upload

---
+++ pengaduan masyarakat/login.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <body bgcolor="EAC696">
+
+        <div>
+        <center><p><big><big><b>LOGIN</B></big></big></big></p></center>
+        <div>
+        <img src="profile.png"
+        style="display:block; margin:auto; widht:150px; height:150px;">
+        </div>
+        <div>
+        <center>
+        <form action= "registrasi.png" mehtod="post">
+            <p>Email: <input type="text" name="email" id=""></p>
+            <P>Password: <input type="text" name="password" id=""></P>
+            <button type="submit">Login</button>
+            <p>Belum Punya Akun?
+                <a href="registrasi.php">Daftar disini</a>
+            </p>
+        </form>
+        </center>
+        </div>
+    </body>
+</body>
+</html>
---
+++ pengaduan masyarakat/registrasi.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <body bgcolor="EAC696">
+    <h1><b><center>REGISTRASI</center></b></h1>
+    <center>
+        <div>
+        <img src="registration.png"
+        style="display:block; margin:auto; widht:150px; height:150px;">
+    <center>
+    </div>
+    <form action="login.php" method="post">
+        <p>Nik: <input type="text" name="nik" id=""></p>
+        <p>Nama: <input type="text" name="nama" id=""></p>
+        <p>Username: <input type="text" name="username" id=""></p>
+        <p>Telepon: <input type="text" name="Telepon" id=""></p>
+        <button type="submit">Daftar</button>
+    </form>
+</body>
+</body>
+</center>
