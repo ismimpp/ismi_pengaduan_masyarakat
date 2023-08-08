0d5e9e82f74d3c2a5fce7d0a34cf49613ac2c303
ismimpp <133618672+ismimpp@users.noreply.github.com>
2023-08-08 09:39:37 +0700

    Create home.php

---
+++ pengaduan masyarakat/home.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <Link rel="stylesheet" href="bs/bs/css/bootstrap.min.css">
+</head>
+<body>
+<nav class="navbar navbar-expand-lg" style="background-color: #7A9D54;">
+    <a class="navbar-brand" href="#">Pengaduan Masyarakat</a>
+ 
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+ 
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+ 
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Mayarakat</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Login</a>
+        </li>
+      </ul>
+ 
+      <form class="form-inline my-2 my-lg-0">
+        <input class="form-control mr-sm-2" type="search" placeholder="search" aria-label="search">
+      </form>
+ 
+   </div>
+</div>
+ 
+</nav>
+<nav class="navbar navbar-expand-lg navbar-light navbar-light">
+<div class="container">
+<table class="table">
+  <thead>
+    <tr>
+      <th scope="col">NO</th>
+      <th scope="col">id</th>
+      <th scope="col">tanggal</th>
+      <th scope="col">nik</th>
+      <th scope="col">isi laporan</th>
+      <th scope="col">foto</th>
+      <th scope="col">status</th>
+
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>111</td>
+      <td>08/18/23</td>
+      <td>12345</td>
+      <td>longsor</td>
+      <td>
+        <img src="longsor.jpg" style="height:80px; weight:80px;">
+      </td>
+      <td>diproses</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>222</td>
+      <td>11/09/23</td>
+      <td>54321</td>
+      <td>gempa bumi</td>
+      <td>
+        <img src="gempa.jpg" style="height:80px; weight:80px;">
+      </td>
+      <td>diterima</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td></td>
+      <td></td>
+    </tr>
+  </tbody>
+</table>
+</div>
+</body>
+</html>
